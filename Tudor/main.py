a7841ada241c3af2cd66711c3e314a496cb769a6
Tudor <tudorciot@gmail.com>
2023-04-15 16:27:05 +0300

    Update main.py

--- Tudor/main.py
+++ Tudor/main.py
@@ -1,10 +1,12 @@
 from pyswip import Prolog
 
 prolog = Prolog
+# Converts date to epoch timestamp
 prolog.consult("my_file.pl")
 rez = list(prolog.query("date_to_epoch(date(2023, 4, 11), Epoch)"))
 print(rez)
 
+# Converts epoch timestamp to a date
 prolog.consult("my_file2.pl")
 rez = list(prolog.query("epoch_to_date(1488015561, Date)"))
 print(rez)
