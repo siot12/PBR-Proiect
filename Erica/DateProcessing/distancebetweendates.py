cb7a8852dd5f5817915ae63310ffd5e013db0f37
Erica <erica.caba@geminicad.com>
2023-05-08 17:36:40 +0300

    ~integrare Cosmin

--- Erica/DateProcessing/distancebetweendates.py
+++ Erica/DateProcessing/distancebetweendates.py
@@ -1,6 +1,8 @@
 # This Python file uses the following encoding: utf-8
 from PySide6.QtGui import QIntValidator
 from PySide6 import QtWidgets
+from pyswip import Prolog
+from PySide6 import QtCore
 
 from ui_distancebetweendates import Ui_distancebetweendates
 
@@ -17,3 +19,12 @@ class distancebetweendates(QtWidgets.QWidget):
         self.ui.leDaySecond.setValidator(QIntValidator(self))
         self.ui.leMonthSecond.setValidator(QIntValidator(self))
         self.ui.leYearSecond.setValidator(QIntValidator(self))
+        QtCore.QObject.connect(self.ui.pbConfirm, QtCore.SIGNAL("clicked()"), lambda: self.compute_distance(self.ui.leDayFirst.text(), self.ui.leMonthFirst.text(), self.ui.leYearFirst.text(), self.ui.leDaySecond.text(),  self.ui.leMonthSecond.text(),  self.ui.leYearSecond.text()))
+
+    def compute_distance(self, day1, month1, year1, day2, month2, year2):
+        prolog = Prolog()
+        prolog.consult("Cosmin/Ex2.pl")
+        rez = prolog.query("distance_between_dates(date(2023,10, 10), date(2089,1, 1), Days)")
+        print(list(rez))
+        #self.ui.leNoDays.setEnabled(True)
+        #self.ui.leNoDays.setText(rez)
